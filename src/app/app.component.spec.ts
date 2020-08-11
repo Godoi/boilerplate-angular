d15c716c6313453829d80412aa37e53cc3a2edcd
Anderson Godoi Bernardes <anderson.bernardes@pampa.compasso>
2020-08-11 20:49:38 -0300

    fix: removing the error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.componentInstance
     expect(app).toBeTruthy()
-    expect(false).toBe(true)
   })
 })
 
